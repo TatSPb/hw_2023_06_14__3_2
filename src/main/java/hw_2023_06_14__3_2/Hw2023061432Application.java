211835209a09e7bd606e24674f68709f957a3edd
TatSPb <t-sverchkova@yandex.ru>
2023-07-12 16:31:53 +0300

    Initial commit

---
+++ src/main/java/hw_2023_06_14__3_2/Hw2023061432Application.java
@@ -0,0 +1,13 @@
+package hw_2023_06_14__3_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2023061432Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw2023061432Application.class, args);
+    }
+
+}
